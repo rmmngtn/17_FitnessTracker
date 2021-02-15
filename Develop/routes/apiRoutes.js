14feec9a5f80562bfd45adf5fa50203e2308d8e4
Remmington Pascone <remmingtonpearce@gmail.com>
2021-02-15 13:13:42 -0800

    initial additions from starter code

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router(); 
+const Workout = require('../models/workout'); 
+
+
+
+
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 const express = require("express"); 
-// add mongoose 
+const Mongoose = require("mongoose"); 
+const logger = require("morgan"); 
 
 
 
-const PORT = process.env.PORT || 3002;
-
+const PORT = 3002;
 const app = express();
+
+app.use(logger("dev"));
+
 
 app.use(express.urlencoded({extended: true})); 
 app.use(express.json());
 
 app.use(express.static("public")); 
 
+Mongoose.connect("mongodb://localhost/workout", { 
+  useNewUrlParser: true, 
+  useFindAndModify: false
+}); 
+
+
+app.use(require("./routes/htmlRoutes")); 
+
+
 
 app.listen(PORT, () => {
     console.log(`App running on http://localhost:${PORT}!`);
-  });
+  });3
