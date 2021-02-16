1c2aa07fc3e8e8a49ee6b7ec5eaefeb5319a4285
Remmington Pascone <remmingtonpearce@gmail.com>
2021-02-15 20:29:18 -0800

    added await and async to functions, need to figure out aggregates

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,26 +3,28 @@ const Workout = require('../models/workoutsController');
 
 
 // gets saved/most recent workouts 
-router.get("/api/workouts", (req, res) => {
-    Workout.find({}, (err, data) => {
-    res.json(data)
-    })
-})
+// router.get("/api/workouts", async (req, res) => {
+//     const newWorkout = await Workout.find({}, (err, data) => {
+//     res.json(data)
+//     })
+// })
 
-router.post("/api/workouts", (req, res) => { 
-    Workout.create({}, (err, data) => 
-    res.json(data))
-})
+router.post("/api/workouts", async (req, res) => { 
+    const newWorkout = await Workout.create({}) 
+    res.json(newWorkout)
+    
+}); 
 
-router.put("/api/workouts/:id", (req, res) => { 
-    Workout.findOneAndUpdate(
+router.put("/api/workouts/:id", async (req, res) => { 
+    const updateWorkout = await Workout.findOneAndUpdate(
         {_id: req.params.id}, 
         { 
             $push: { 
                 exercises: req.body 
             }, 
-            
-        })
+
+        }); 
+        res.json(updateWorkout); 
 })
 
 
