0b0419f842eeadc9f9dd6a307e7c7dbb2e983f5d
Remmington Pascone <remmingtonpearce@gmail.com>
2021-02-16 20:13:30 -0800

    finished routes, dashboard populates, tracing errors

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,30 +1,79 @@
-const router = require('express').Router(); 
-const Workout = require('../models/workoutsController'); 
+const router = require('express').Router();
+const Workout = require('../models/workoutsController');
 
 
-// gets saved/most recent workouts 
-router.get("/api/workouts", async (req, res) => {
-    const newWorkout = await Workout.find({}, (err, data) => {
-    res.json(data)
+// // gets saved/most recent workouts 
+// router.get("/api/workouts", (req, res) => {
+//      Workout.find({}, (err, data) => {
+//         res.json(data)
+//     })
+// })
+
+
+router.get("/api/workouts", (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { $sum: "$exercises.duration" }
+            }
+        }
+    ]).then(data => {
+        res.json(data);
     })
+        .catch(err => {
+            res.json(err);
+        });
 })
 
-router.post("/api/workouts", async (req, res) => { 
-    const newWorkout = await Workout.create({}) 
-    res.json(newWorkout);
-    
-}); 
+// post new workout
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
 
-router.put("/api/workouts/:id", async (req, res) => { 
-    const updateWorkout = await Workout.findOneAndUpdate(
-        {_id: req.params.id}, 
-        { 
-            $push: { 
-                exercises: req.body 
-            }, 
+// update workout
+router.put("/api/workouts/:id", (req, res) => {
+    Workout.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+            $push: {
+                exercises: req.body
+            },
+            $inc: {
+                totalDuration: req.body.duration
+            }
+        }, {
+        new: true,
+    }).then(data => {
+        res.json(data);
+    })
+        .catch(err => {
+            res.json(err);
+        });
+})
 
-        }); 
-        res.json(updateWorkout); 
+// 7 day range of workouts 
+router.get("/api/workouts/range", (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { $sum: "$exercises.duration" }
+            }
+        }
+    ])
+        .sort({ day: -1 })
+        .limit(7)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.json(err);
+        });
 })
 
 
